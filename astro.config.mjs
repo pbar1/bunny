4127214a79e8fb219a150e2ef877b82585e9da5c
Pierce Bartine <piercebartine@gmail.com>
2023-04-14 17:29:08 -0700

    Add table and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://pbar1.github.io",
-  base: "bunny"
+  base: "bunny",
+  integrations: [tailwind()]
 });
